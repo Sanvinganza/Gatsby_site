117a13414d9ee375d51bd93c124a39e52ee58141
Polina <polina.rozhko.blr@gmail.com>
2021-11-04 13:19:28 +0300

    [fix]-add direct link to mongoose connect

--- server/src/config/mongoose.ts
+++ server/src/config/mongoose.ts
@@ -1,18 +1,19 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || process.env.MONGO_DB_LOCAL_URL;
+// const MONGO_DB_URL = process.env.MONGO_DB_URL || process.env.MONGO_DB_LOCAL_URL;
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+// const options = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
 
+const MONGO_DB_URL = 'mongodb://localhost:27017/';
 const mongooseConnect = async () => {
   try {
-    await mongoose.connect(MONGO_DB_URL, options);
+    await mongoose.connect(MONGO_DB_URL);
     console.log(`✅ MongoDB is connected ${MONGO_DB_URL}`);
   } catch (error) {
     console.log(`❌ Not Connected to MongoDB' + ${error}`);
